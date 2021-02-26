4de2abb458c98d421da013474a4bee9e8cbf77b0
JiseonYu <jiseon477@gmail.com>
2021-02-25 22:30:09 -0500

    unit2 complate

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jiseon Yu on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
